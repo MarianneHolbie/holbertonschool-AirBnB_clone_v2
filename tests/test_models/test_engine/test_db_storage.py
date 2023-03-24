4c0df2a57c434ad884a953d769dfdd4565c19ba7
Vanessa Tessier <113889290+TessierV@users.noreply.github.com>
2023-03-24 10:19:09 +0100

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -10,10 +10,6 @@ from MySQLdb import _mysql
 
 class test_dbStorage(unittest.TestCase):
     """Class for testing db_storage"""
-    def test_dbstorage(self):
-        """Testing db_storage"""
-        self.assertEqual(1, 1)
-    
     def test_all(self):
         """ test all() function """
         new = City()
@@ -23,12 +19,9 @@ class test_dbStorage(unittest.TestCase):
 
     def test_methods(self):
         """ test presence of methods """
-        self.assertTrue(hasattr(storage, 'all'))
         self.assertTrue(hasattr(storage, 'new'))
-        self.assertTrue(hasattr(storage, 'save'))
         self.assertTrue(hasattr(storage, 'delete'))
-        self.assertTrue(hasattr(storage, 'reload'))
-
+        
     def test_models(self):
         """ test of creating a models user with db """
         db=_mysql.connect("localhost", "hbnb_test", "hbnb_test_pwd", "hbnb_test_db")
