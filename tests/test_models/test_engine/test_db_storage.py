3a20e8aedba71f67f1d1661011af98d5bb4da63c
Vanessa Tessier <113889290+TessierV@users.noreply.github.com>
2023-03-24 10:15:01 +0100

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,12 +1,43 @@
 #!/usr/bin/python3
-"""
-Module for testing db_storage
-"""
+""" Module for testing db storage"""
 import unittest
+from models.base_model import BaseModel
+from models import storage
+from models.city import City
+from models.user import User
+import os
+from MySQLdb import _mysql
 
-
-class test_dbstorage(unittest.TestCase):
+class test_dbStorage(unittest.TestCase):
     """Class for testing db_storage"""
     def test_dbstorage(self):
         """Testing db_storage"""
         self.assertEqual(1, 1)
+    
+    def test_all(self):
+        """ test all() function """
+        new = City()
+        storage.save()
+        temp = storage.all()
+        self.assertIsInstance(temp, dict)
+
+    def test_methods(self):
+        """ test presence of methods """
+        self.assertTrue(hasattr(storage, 'all'))
+        self.assertTrue(hasattr(storage, 'new'))
+        self.assertTrue(hasattr(storage, 'save'))
+        self.assertTrue(hasattr(storage, 'delete'))
+        self.assertTrue(hasattr(storage, 'reload'))
+
+    def test_models(self):
+        """ test of creating a models user with db """
+        db=_mysql.connect("localhost", "hbnb_test", "hbnb_test_pwd", "hbnb_test_db")
+        gab = User(name="Name", password="pwd", email="mail@mail.com")
+        gab.save()
+        db.query("""SELECT * FROM users""")
+        r=db.store_result()
+        #print(len(r.fetch_row()))
+        self.assertEqual(1, len(r.fetch_row()))
+        
+if __name__ == "__main__":
+    unittest.main()
