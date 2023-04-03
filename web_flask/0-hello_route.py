6face03e4fff47ec7e7f43c7a50ee7de12782251
Arrue Marianne <5608@holbertonstudents.com>
2023-04-03 01:42:01 -0700

    solve pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """
         Response to request main URL "/" or not in @app.route()
     """
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
