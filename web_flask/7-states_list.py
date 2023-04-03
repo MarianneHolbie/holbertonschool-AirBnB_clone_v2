fb68a918796eb74fa56ee64fab0fef15c3983cd1
Arrue Marianne <5608@holbertonstudents.com>
2023-04-03 07:21:09 -0700

    import also State from models

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,15 +6,10 @@ from flask import Flask
 from markupsafe import escape
 from flask import render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
-@app.teardown_appcontext
-def remove_session():
-    """
-        after each request : remove current SQLAlchemy Session
-    """
-    storage.close()
 
 @app.route("/states_list", strict_slashes=False)
 def list_states():
@@ -25,8 +20,16 @@ def list_states():
             sorted by name(A->Z)
             LI tag : description of one State:<state.id>: <B><state.name></B>
     """
-    states = storage.all()
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def remove_session():
+    """
+        after each request : remove current SQLAlchemy Session
+    """
+    storage.close()
 
 
 if __name__ == '__main__':
