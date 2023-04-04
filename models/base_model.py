29900c152ddb86eb8115a6d828409667fd6963fb
Arrue Marianne <5608@holbertonstudents.com>
2023-04-04 05:11:00 -0700

    update basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,6 @@ class BaseModel:
     """
     id = Column(
         String(60),
-        unique=True,
         nullable=False,
         primary_key=True
     )
@@ -41,53 +40,64 @@ class BaseModel:
     )
 
     """ A base class for all hbnb models """
-    def __init__(self, *args, **kwargs):
-        """ Instantiates a new model """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
 
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
-            # if kwarg (if object exist) with update, create
-            # delete class and update value with value
-            if '__class__' in kwargs.keys():
-                kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            # print(kwargs)
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                setattr(self, key, value)
+            try:
+                kwargs['updated_at'] = datetime.\
+                    strptime(kwargs['updated_at'],
+                             '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.\
+                    strptime(kwargs['created_at'],
+                             '%Y-%m-%dT%H:%M:%S.%f')
                 del kwargs['__class__']
-            else:
-                # if kwargs but not object exist
+            except KeyError:
                 self.id = str(uuid.uuid4())
-                self.created_at = datetime.utcnow()
-                self.updated_at = datetime.utcnow()
-            # update the dict
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+
             self.__dict__.update(kwargs)
 
     def __str__(self):
-        """ Returns a string representation of the instance """
+        """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """ Updates updated_at with current time when instance is changed """
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
-        models.storage.save()
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.new(self)
+        storage.save()
 
     def to_dict(self):
-        """ Convert instance into dict format """
+        """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if "_sa_instance_state" in dictionary.keys():
-            del (dictionary["_sa_instance_state"])
+
+        try:
+            del dictionary["_sa_instance_state"]
+        except Exception:
+            return dictionary
+
         return dictionary
 
     def delete(self):
-        """ Delete the current instance from the storage """
-        models.storage.delete(self)
+        """Module is doc"""
+        from models import storage
+        storage.delete(self)
