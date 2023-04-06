34f796b63c5e737a3ebc0fcbe7af4cff7f24eeb1
Arrue Marianne <5608@holbertonstudents.com>
2023-04-06 05:46:28 -0700

    HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    Create flask application instance (app)
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+
+@app.route('/hbnb_filters', strict_slashes=False, methods=["GET"])
+def hbnb_filters(id=None):
+    """
+        by default no id
+        if id extract information of corresponding id
+    """
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """ remove the current SQLalchemy session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
